d9678ab13a2053f74bd12a03bf7990d268541c1c
Nikhil Kumar Singh <nikhilsinghns01@gmail.com>
2021-12-02 13:08:27 +0530

    Initial Commit

---
+++ App.js
@@ -0,0 +1,52 @@
+import React , {useState} from 'react';
+import {StyleSheet, View , FlatList} from 'react-native';
+import GoalItem from './components/GoalItem';
+import GoalInput from './components/GoalInput';
+
+export default function App() {
+  const [courseGoals,setCourseGoals] = useState([])
+
+  const addGoalHandler = (goalTitle) => {
+    setCourseGoals(currentGoals => [...currentGoals,
+      {id: Math.random().toString() , value:goalTitle }])
+  }
+
+  const removeGoalHandler = goalId => {
+    setCourseGoals(currentGoals => {
+      return currentGoals.filter((goal) => goal.id !== goalId) 
+    })
+  }
+
+  return (
+    <View style={styles.screen}>
+      <GoalInput onAddGoal={addGoalHandler} />
+      <FlatList
+      keyExtractor={(item , index) =>item.id} 
+      data={courseGoals} 
+      renderItem={itemData => <GoalItem  id={itemData.item.id} onDelete={removeGoalHandler} title={itemData.item.value} />} 
+      />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  screen : {
+    padding: 50,
+  }
+});
+
+// export default function App() {
+//   console.log("app executed")
+
+//   return (
+//     <View style={styles.container}>
+//       <Text>Hello React Native</Text>
+//     </View>
+//   )
+// }
+
+// const styles = StyleSheet.create({
+//   container : {
+//     padding: 50
+//   }
+// })
